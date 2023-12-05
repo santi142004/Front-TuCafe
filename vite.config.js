b46cd78ea1025fc70ecf5694f18d05f368f6144c
santi142004 <santiagobarreralopez2004@gmail.com>
2023-12-05 07:22:32 -0500

    cambios en las rutas

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-router-dom'],
+  //   },
+  // },
 };
